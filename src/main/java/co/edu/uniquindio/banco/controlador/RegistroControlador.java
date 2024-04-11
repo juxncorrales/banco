416fee7f9c3a4eedd5e98746bd95d70ce0268dc7
juxncorrales <juanc.corralec@uqvirtual.edu.co>
2024-04-11 13:15:52 -0500

    upload first version

---
+++ src/main/java/co/edu/uniquindio/banco/controlador/RegistroControlador.java
@@ -0,0 +1,85 @@
+package co.edu.uniquindio.banco.controlador;
+
+import co.edu.uniquindio.banco.modelo.Banco;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ * Clase que representa el controlador de la ventana de registro de usuario
+ * @author caflorezvi
+ */
+public class RegistroControlador {
+
+    @FXML
+    private TextField txtIdentificacion;
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtCorreo;
+    @FXML
+    private TextField txtDireccion;
+    @FXML
+    private PasswordField txtPassword;
+
+    private final Banco banco;
+
+    /**
+     * Constructor de la clase, inicializa el banco
+     */
+    public RegistroControlador(){
+        banco = new Banco();
+    }
+
+    /**
+     * Método que se ejecuta al presionar el botón de registrarse
+     * @param actionEvent evento de acción
+     */
+    public void registrarse(ActionEvent actionEvent) {
+
+        try {
+            // Se intenta agregar el usuario al banco
+            banco.agregarUsuario(
+                    txtNombre.getText(),
+                    txtDireccion.getText(),
+                    txtIdentificacion.getText(),
+                    txtCorreo.getText(),
+                    txtPassword.getText() );
+
+            // Se intenta agregar una cuenta de ahorros al usuario
+            banco.agregarCuentaAhorros(txtIdentificacion.getText(), 0F);
+
+            // Se muestra un mensaje de éxito y se cierra la ventana
+            crearAlerta("Usuario registrado correctamente", Alert.AlertType.INFORMATION);
+            cerrarVentana();
+
+        }catch (Exception e){
+            crearAlerta(e.getMessage(), Alert.AlertType.ERROR);
+        }
+
+    }
+
+    /**
+     * Método que se encarga de mostrar una alerta en pantalla
+     * @param mensaje mensaje a mostrar
+     * @param tipo tipo de alerta
+     */
+    public void crearAlerta(String mensaje, Alert.AlertType tipo){
+        Alert alert = new Alert(tipo);
+        alert.setTitle("Alerta");
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+    /**
+     * Método que se encarga de cerrar la ventana actual
+     */
+    public void cerrarVentana(){
+        Stage stage = (Stage) txtIdentificacion.getScene().getWindow();
+        stage.close();
+    }
+}
